3075966d30386f646b6be40f9cbfab73e7e9e1d4
Demries <demries@yandex.ua>
2017-01-17 16:14:38 +0200

    Запрограммировано всплывающая форма.

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,55 @@ function initMap() {
 
 var hotelSearchBtn = document.querySelector('#hotel-search-btn'),
     popup = document.querySelector('.modal-content-hotel-search'),
-    dateCheckInInput = popup.querySelector('#date-check-in');
+    form = popup.querySelector('form'),
+    dateCheckIn = form.querySelector('#date-check-in'),
+    dateLeaving = form.querySelector('#date-leaving'),
+    increaseBtn = form.querySelectorAll('.increase-btn'),
+    reduceBtn = form.querySelectorAll('.reduce-btn');
+
+var currentDate = new Date(); // Сегоднешняя дата
 
 hotelSearchBtn.addEventListener('click', function(e) {
     e.preventDefault();
 
+    popup.classList.remove('modal-error');
     popup.classList.toggle('modal-content-show');
-    dateCheckInInput.focus();
+
+    dateCheckIn.value = currentDate.getFullYear() + '-' + ( (currentDate.getMonth() + 1 < 10) ? '0' + (currentDate.getMonth() + 1) : currentDate.getMonth() ) + '-' + currentDate.getDate();
+    dateLeaving.focus();
+});
+
+form.addEventListener('submit', function(e) {
+    if ( !dateCheckIn.value || !dateLeaving.value ) {
+        e.preventDefault();
+
+        popup.classList.remove('modal-error');
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add('modal-error');
+        console.log('Нужно ввести дату заезда и дата выезда');
+    }
+});
+
+// Закрытие модального окна по нажатию на Esc
+window.addEventListener('keydown', function(e) {
+    if ( e.keyCode === 27 && popup.classList.contains('modal-content-show') ) {
+        popup.classList.remove('modal-content-show');
+        popup.classList.remove('modal-error');
+    }
+});
+
+Array.prototype.forEach.call(increaseBtn, function(increaseBtn) {
+    increaseBtn.addEventListener('click', function(e) {
+        var input = this.nextElementSibling;
+
+        if ( input.value >= 0 ) input.value++;
+    });
+});
+
+Array.prototype.forEach.call(reduceBtn, function(reduceBtn) {
+    reduceBtn.addEventListener('click', function(e) {
+        var input = this.previousElementSibling;
+
+        if ( input.value > 0 ) input.value--;
+    });
 });
